dac39ad758c12b5beeb486a5e5688311784ceaa5
Dillon_Farber <55909397+DillonFarber@users.noreply.github.com>
2022-04-23 14:01:29 -0500

    working on hw, added threads

--- unix/hw/hw7.c
+++ unix/hw/hw7.c
@@ -1,1 +1,24 @@
 
+/*
+    NAME: Dillon Farber
+    Course Number / Name: 4350-251 Unix System Programming
+    Assignment Number: 7
+    Due Date: April 28, 2022
+*/
+
+
+#include <pthred.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+pthread_mutex_t lock;
+int RESULT;
+
+int main() 
+{
+    pthread_t thread1, thread2, thread3, thread4, thread5 thread6, thread7, thread8, thread9, thread10, thread11, thread12;
+    int userNum;
+
+    
+
+}
